66570650c4795cff8b524ea048cb043ee7351765
tk42 <nsplat@gmail.com>
2022-08-31 14:34:37 +0000

    Added build module

---
+++ st-excel-table/frontend/build/precache-manifest.fedbf37920b3d6d5cc51e22af515ee11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d40c0153321492076f142539f0a922",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d22f6ab0dffb60f1523",
+    "url": "./static/js/2.8b8c5d06.chunk.js"
+  },
+  {
+    "revision": "c84fe3a6fba229bd8a4fa43bc50a834a",
+    "url": "./static/js/2.8b8c5d06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39beef67555d05216909",
+    "url": "./static/js/main.240f20b2.chunk.js"
+  },
+  {
+    "revision": "ddc0798ca4473527d7d9",
+    "url": "./static/js/runtime-main.4eedf725.js"
+  }
+]);
